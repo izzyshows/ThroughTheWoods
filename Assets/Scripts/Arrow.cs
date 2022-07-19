4943e4094fa2ec0011d30e645a574f31274a5ff0
izzyshows <101434643+izzyshows@users.noreply.github.com>
2022-07-19 14:51:38 -0400

    add arrow and shoot mechanic

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    public Animator animator;
+    public int direction;
+    //four directions 0,1,2,3
+    //        down, left, up, right
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private Vector2 dirVector;
+
+    private void FixedUpdate() {
+        if (direction == 0) dirVector = new Vector2(0.0f, -1.0f);
+        if (direction == 1) dirVector = new Vector2(-1.0f, 0.0f);
+        if (direction == 2) dirVector = new Vector2(0.0f, 1.0f);
+        if (direction == 3) dirVector = new Vector2(1.0f, 0.0f);
+
+            rb.velocity = dirVector.normalized * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Arrow"){
+            Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
+            return;
+        }
+
+        Vector2 newPosition = new Vector2(
+            transform.position.x - dirVector.x * 0.1f,
+            transform.position.y - dirVector.y * 0.1f);
+
+            rb.MovePosition(newPosition);
+            speed = 0f;
+    }
+}
--- Assets/Scripts/LucindaController.cs
+++ Assets/Scripts/LucindaController.cs
@@ -7,8 +7,13 @@ public class LucindaController : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     public Animator animator;
+    public GameObject shot;
+    private int direction;
+    private float zrot;
+    private Arrow arrow;
 
     private void Start() {
+        direction = 0;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -18,5 +23,40 @@ public class LucindaController : MonoBehaviour
             Input.GetAxisRaw("Vertical"));
             rb.velocity = moveInput.normalized * speed;
             animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+
+    private void Update() {
+
+        float x, y;
+        x = rb.velocity.x;
+        y = rb.velocity.y;
+        if (x != 0 || y != 0) {
+            if (y < x) if (y < -x) direction = 0;
+            if (y > x) if (y < -x) direction = 1;
+            if (y > x) if (y > -x) direction = 2;
+            if (y < x) if (y > -x) direction = 3;
+        }
+
+
+        if (Input.GetKeyDown("space")) {
+
+            if (direction == 0) zrot = -90f;
+            if (direction == 1) zrot = 180f;
+            if (direction == 2) zrot = 90f;
+            if (direction == 3) zrot = 0f;
+
+
+            GameObject ar = Instantiate(
+                shot,
+                new Vector3 (
+                    transform.position.x,
+                    transform.position.y, 1.0f),
+                    Quaternion.Euler(0,0,zrot)
+                
+            );
+
+            ar.GetComponent<Arrow>().direction = direction;
+            if (x != 0|| y != 0) ar.GetComponent<Arrow>().speed += speed;
+        }
     }
 }
